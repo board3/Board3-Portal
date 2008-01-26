7be9f950c5e7ca69248c954c5ce9a518b194e3ba
Kevin <kevin@breest.info>
2008-01-26 11:10:06 +0000

    Unreported: Ice has to be as Dev in the file headers :)

--- portal/block/main_menu.php
+++ portal/block/main_menu.php
@@ -4,7 +4,7 @@
 *
 * @package - Board3portal
 * @version $Id$
-* @copyright (c) kevin / saint ( http://www.board3.de/ ), (c) nickvergessen ( http://mods.flying-bits.org/ ), (c) redbull254 ( http://www.digitalfotografie-foren.de )
+* @copyright (c) kevin / saint ( http://www.board3.de/ ), (c) Ice, (c) nickvergessen ( http://mods.flying-bits.org/ ), (c) redbull254 ( http://www.digitalfotografie-foren.de )
 * @license http://opensource.org/licenses/gpl-license.php GNU Public License 
 *
 */
