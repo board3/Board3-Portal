62ac951a97b4352d2493d83324a6f8f897984806
Kevin <kevin@breest.info>
2008-01-30 23:01:26 +0000

    Unreported: offer a link to the login, if "installer needs founder" message appears. otherwise, with already edited .htaccess, the people could get stuck with a sql error on the portal

--- language/en/mods/portal_install.php
+++ language/en/mods/portal_install.php
@@ -32,55 +32,53 @@ if (empty($lang) || !is_array($lang))
 
 $lang = array_merge($lang, array(
 
-	'INSTALLER_MENU'					=> 'PInUp Menu',
+	'INSTALLER_MENU'						=> 'PInUp Menu',
 	'INSTALLER_MENU_START'				=> 'Start',
-	'INSTALLER_UNINSTALL'				=> 'Uninstall',
-	'INSTALLER_UPDATE'					=> 'Update',
-	'INSTALLER_INSTALL'					=> 'Install',
+	'INSTALLER_UNINSTALL'					=> 'Uninstall',
+	'INSTALLER_UPDATE'						=> 'Update',
+	'INSTALLER_INSTALL'						=> 'Install',
 
-	'INSTALLER_INTRO_TITLE'					=> 'Portal Install/Update Utility',
-	'INSTALLER_INTRO_NOTE'			=> 'Welcome to the Portal Install/Update Utility, hereby known as PInUp',
+	'INSTALLER_INTRO_TITLE'				=> 'Portal Install/Update Utility',
+	'INSTALLER_INTRO_NOTE'				=> 'Welcome to the Portal Install/Update Utility, hereby known as PInUp',
 
-	'INSTALLER_MENU_DONE'				=> 'Latest Version',
+	'INSTALLER_MENU_DONE'					=> 'Latest Version',
 	'INSTALLER_MENU_DONE_TEXT'			=> 'You already have version %s installed, please delete the install_portal folder and return to your <a href="%s">forums</a>.',
 
-	'INSTALLER_INSTALL_TITLE'			=> 'PInUp Install',
-	'INSTALLER_INSTALL_NOTE'			=> 'When you choose to install the MOD, any database of previous versions will be dropped.',
-	'INSTALLER_INSTALL_MENU'			=> 'Installmenu',
+	'INSTALLER_INSTALL_TITLE'				=> 'PInUp Install',
+	'INSTALLER_INSTALL_NOTE'				=> 'When you choose to install the MOD, any database of previous versions will be dropped.',
+	'INSTALLER_INSTALL_MENU'				=> 'Installmenu',
 	'INSTALLER_INSTALL_SUCCESSFUL'		=> 'Installation of the MOD v%s was successful.',
 	'INSTALLER_INSTALL_UNSUCCESSFUL'	=> 'Installation of the MOD v%s was <strong>not</strong> successful.',
 	'INSTALLER_INSTALL_VERSION'			=> 'Install MOD v%s',
 	'INSTALLER_INSTALL_START'			=> 'Please click <a href="%s">Install</a> to start the install utility.',
 
-	'INSTALLER_UPDATE_TITLE'			=> 'PInUp Update',
+	'INSTALLER_UPDATE_TITLE'				=> 'PInUp Update',
 	'INSTALLER_UPDATE_NOTE'				=> 'Update MOD from v%s to v%s',
 
 	'INSTALLER_UNINSTALL_TITLE'			=> 'PInUp Uninstall',
-	'INSTALLER_UNINSTALL_NOTE'		=> 'Welcome to the Updatemenu',
+	'INSTALLER_UNINSTALL_NOTE'			=> 'Welcome to the Updatemenu',
 	'INSTALLER_UNINSTALL_SUCCESSFUL'	=> 'Installation of the MOD v%s was successful.',
 
+	'INSTALLER_NEEDS_FOUNDER'			=> 'You must be logged in as a founder.<br /><a href="/ucp.php?mode=login"><strong>Go to login</strong>',
 
-
-	'INSTALLER_NEEDS_FOUNDER'			=> 'You must be logged in as a founder.',
-
-	'INSTALLER_UPDATE'					=> 'Update',
+	'INSTALLER_UPDATE'						=> 'Update',
 	'INSTALLER_UPDATE_MENU'				=> 'Updatemenu',
 	'INSTALLER_UPDATE_NOTE'				=> 'Update MOD from v%s to v%s',
 	'INSTALLER_UPDATE_SUCCESSFUL'		=> 'Update of the MOD from v%s to v%s was successful.',
-	'INSTALLER_UPDATE_UNSUCCESSFUL'		=> 'Update of the MOD from v%s to v%s was <strong>not</strong> successful.',
+	'INSTALLER_UPDATE_UNSUCCESSFUL'	=> 'Update of the MOD from v%s to v%s was <strong>not</strong> successful.',
 	'INSTALLER_UPDATE_VERSION'			=> 'Update MOD from v%s',
-	'INSTALLER_UPDATE_TO'				=> 'Update to',
-	'INSTALLER_UPDATE_START'			=> 'Please click <a href="%s">Update</a> to start the update utility.',
+	'INSTALLER_UPDATE_TO'					=> 'Update to',
+	'INSTALLER_UPDATE_START'				=> 'Please click <a href="%s">Update</a> to start the update utility.',
 
 	'INSTALLER_UNINSTALL_OLDVERSION'	=> 'Sorry, PInUp does not support the uninstallation of the origonal phpBB3 Portal.',
 
-	'INSTALLER_ERROR'					=> 'PInUp Error',
+	'INSTALLER_ERROR'						=> 'PInUp Error',
 
 	'INSTALLER_USEFUL_INFO'				=> 'Please delete the /install_portal directory.',
 
 	'INSTALLER_UNINSTALL_USEFUL_INFO'	=> 'Remember to delete the portal files and remove the file edits.',
 
-	'WARNING'							=> 'Warning',
+	'WARNING'									=> 'Warning',
 ));
 
 ?>
