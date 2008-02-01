75071e989c949c0ad40e09d39da253bec15b6820
Ice <ice@tcoa.co.uk>
2008-02-01 21:29:27 +0000

    Unreported: Make config_value text not varchar(255)

--- install_portal/schemas/_firebird_schema.sql
+++ install_portal/schemas/_firebird_schema.sql
@@ -6,7 +6,7 @@
 # Table: 'phpbb_portal_config'
 CREATE TABLE phpbb_portal_config (
 	config_name VARCHAR(255) CHARACTER SET NONE DEFAULT '' NOT NULL,
-	config_value VARCHAR(255) CHARACTER SET NONE DEFAULT '' NOT NULL
+	config_value BLOB SUB_TYPE TEXT CHARACTER SET UTF8 DEFAULT '' NOT NULL
 );;
 
 ALTER TABLE phpbb_portal_config ADD PRIMARY KEY (config_name);;
--- install_portal/schemas/_mssql_schema.sql
+++ install_portal/schemas/_mssql_schema.sql
@@ -12,7 +12,7 @@ GO
 */
 CREATE TABLE [phpbb_portal_config] (
 	[config_name] [varchar] (255) DEFAULT ('') NOT NULL ,
-	[config_value] [varchar] (255) DEFAULT ('') NOT NULL 
+	[config_value] [text] DEFAULT ('') NOT NULL 
 ) ON [PRIMARY]
 GO
 
