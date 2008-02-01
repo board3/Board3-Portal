6716322819f9700fe31487ebcc7915ea2269b2f4
Ice <ice@tcoa.co.uk>
2008-02-01 21:29:27 +0000

    Unreported: Make config_value text not varchar(255)

--- root/install_portal/schemas/_mysql_40_schema.sql
+++ root/install_portal/schemas/_mysql_40_schema.sql
@@ -5,7 +5,7 @@
 # Table: 'phpbb_portal_config'
 CREATE TABLE phpbb_portal_config (
 	config_name varbinary(255) DEFAULT '' NOT NULL,
-	config_value varbinary(255) DEFAULT '' NOT NULL,
+	config_value mediumblob NOT NULL,
 	PRIMARY KEY (config_name)
 );
 
--- root/install_portal/schemas/_mysql_41_schema.sql
+++ root/install_portal/schemas/_mysql_41_schema.sql
@@ -5,7 +5,7 @@
 # Table: 'phpbb_portal_config'
 CREATE TABLE phpbb_portal_config (
 	config_name varchar(255) DEFAULT '' NOT NULL,
-	config_value varchar(255) DEFAULT '' NOT NULL,
+	config_value mediumtext NOT NULL,
 	PRIMARY KEY (config_name)
 ) CHARACTER SET `utf8` COLLATE `utf8_bin`;
 
