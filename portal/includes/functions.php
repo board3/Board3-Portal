34c87a4d9630c0a7455e99339716d790592d15de
Ice <ice@tcoa.co.uk>
2008-02-02 22:05:21 +0000

    News wrong way round

--- portal/includes/functions.php
+++ portal/includes/functions.php
@@ -298,7 +298,7 @@ function phpbb_fetch_posts($forum_from, $number_of_posts, $text_length, $time, $
 					LEFT JOIN
 						' . POSTS_TABLE . ' as p
 					ON
-						t.topic_first_post_id = p.post_id
+						t.topic_last_post_id = p.post_id
 					WHERE
 						' . $topic_type . '
 						' . $post_time . '
