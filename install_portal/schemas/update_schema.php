1c99e658acb922a26f4ba2e5b1f7cd749a46ff78
Ice <ice@tcoa.co.uk>
2008-02-02 13:55:04 +0000

    Corrected Alter

--- install_portal/schemas/update_schema.php
+++ install_portal/schemas/update_schema.php
@@ -1,6 +1,7 @@
 <?php
 
 $sql_update['0.2.0'] = array(
+	"ALTER TABLE phpbb_portal_config CHANGE config_value config_value text NOT NULL",
 	"INSERT INTO phpbb_portal_config (config_name, config_value) VALUES ('portal_poll_limit', '3')",
 	"INSERT INTO phpbb_portal_config (config_name, config_value) VALUES ('portal_poll_allow_vote', '1')",
 	"INSERT INTO phpbb_portal_config (config_name, config_value) VALUES ('portal_birthdays_ahead', '7')",
