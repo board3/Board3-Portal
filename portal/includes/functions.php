de3445a65536db9706cd48502ea20db246a36e0e
Ice <ice@tcoa.co.uk>
2008-02-02 22:13:20 +0000

    There we goooooo right order and right user now.

--- portal/includes/functions.php
+++ portal/includes/functions.php
@@ -290,7 +290,7 @@ function phpbb_fetch_posts($forum_from, $number_of_posts, $text_length, $time, $
 					LEFT JOIN
 						' . USERS_TABLE . ' as u
 					ON
-						t.topic_poster = u.user_id
+						t.topic_last_poster_id = u.user_id
 					LEFT JOIN
 						' . FORUMS_TABLE . ' as f
 					ON
