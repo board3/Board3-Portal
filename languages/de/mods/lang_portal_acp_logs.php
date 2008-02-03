c8c706d17b162488322fb2774b5a0a2b54c53258
Kevin <kevin@breest.info>
2008-02-03 21:43:30 +0000

    * @based on: phpBB3 Portal by Sevdin Filiz, www.phpbb3portal.com

--- languages/de/mods/lang_portal_acp_logs.php
+++ languages/de/mods/lang_portal_acp_logs.php
@@ -3,8 +3,9 @@
 /**
 *
 * @package - Board3portal
-* @version
-* @copyright (c) kevin / saint ( http://www.board3.de/ ), (c) Ice, (c) nickvergessen ( http://mods.flying-bits.org/ ), (c) redbull254 ( http://www.digitalfotografie-foren.de )
+* @version $Id$
+* @copyright (c) kevin / saint ( http://www.board3.de/ ), (c) Ice, (c) nickvergessen ( http://www.flying-bits.org/ ), (c) redbull254 ( http://www.digitalfotografie-foren.de )
+* @based on: phpBB3 Portal by Sevdin Filiz, www.phpbb3portal.com
 * @license http://opensource.org/licenses/gpl-license.php GNU Public License 
 *
 */
