4a26c35f2f4bccc4946dd1691103dc2ec960ce28
Kevin <kevin@breest.info>
2008-02-03 21:43:30 +0000

    * @based on: phpBB3 Portal by Sevdin Filiz, www.phpbb3portal.com

--- portal/block/change_style.php
+++ portal/block/change_style.php
@@ -4,7 +4,8 @@
 *
 * @package - Board3portal
 * @version $Id$
-* @copyright (c) kevin / saint ( http://www.board3.de/ ), (c) Ice, (c) nickvergessen ( http://mods.flying-bits.org/ ), (c) redbull254 ( http://www.digitalfotografie-foren.de )
+* @copyright (c) kevin / saint ( http://www.board3.de/ ), (c) Ice, (c) nickvergessen ( http://www.flying-bits.org/ ), (c) redbull254 ( http://www.digitalfotografie-foren.de )
+* @based on: phpBB3 Portal by Sevdin Filiz, www.phpbb3portal.com
 * @license http://opensource.org/licenses/gpl-license.php GNU Public License 
 *
 */
