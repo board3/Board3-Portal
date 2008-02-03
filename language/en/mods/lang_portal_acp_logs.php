7efd2c831ec793ea83054618a0b00f5536bb5280
Kevin <kevin@breest.info>
2008-02-03 21:44:36 +0000

    missing version

--- language/en/mods/lang_portal_acp_logs.php
+++ language/en/mods/lang_portal_acp_logs.php
@@ -3,7 +3,7 @@
 /**
 *
 * @package - Board3portal
-* @version
+* @version $Id$
 * @copyright (c) kevin / saint ( http://www.board3.de/ ), (c) Ice, (c) nickvergessen ( http://www.flying-bits.org/ ), (c) redbull254 ( http://www.digitalfotografie-foren.de )
 * @based on: phpBB3 Portal by Sevdin Filiz, www.phpbb3portal.com
 * @license http://opensource.org/licenses/gpl-license.php GNU Public License 
