7574e557a2b5fb434929067902dd11e6353df53b
Ice <ice@tcoa.co.uk>
2008-04-27 13:08:22 +0000

    Install update

--- root/install_portal/schemas/update_schema.php
+++ root/install_portal/schemas/update_schema.php
@@ -11,7 +11,7 @@ $sql_update['0.2.2'] = array(
 	"INSERT INTO phpbb_portal_config (config_name, config_value) VALUES ('portal_attachments_forum_ids', '')",
 );
 
-$sql_update['0.2.3'] = array(
+$sql_update['0.3.0'] = array(
 	"INSERT INTO phpbb_portal_config (config_name, config_value) VALUES ('portal_announcements_permissions', '1')",
 	"INSERT INTO phpbb_portal_config (config_name, config_value) VALUES ('portal_news_permissions', '1')",
 	"INSERT INTO phpbb_portal_config (config_name, config_value) VALUES ('portal_custom_center', '0')",
@@ -28,5 +28,26 @@ $sql_update['0.2.3'] = array(
 	"INSERT INTO phpbb_portal_config (config_name, config_value) VALUES ('portal_announcements_archive', '1')",
 	"INSERT INTO phpbb_portal_config (config_name, config_value) VALUES ('portal_links_array', 'a:2:{i:1;a:2:{s:4:\"text\";s:9:\"Board3.de\";s:3:\"url\";s:21:\"http://www.board3.de/\";}i:2;a:2:{s:4:\"text\";s:9:\"phpBB.com\";s:3:\"url\";s:21:\"http://www.phpbb.com/\";}}')",
 );
+
+$mod_update['0.3.0'] = array(
+	'customblock' => array(
+		'module_basename'   => 'portal',
+		'module_enabled'   => 1,
+		'module_display'   => 1,
+		'module_class'      => 'acp',
+		'module_langname'   => 'ACP_PORTAL_CUSTOM_INFO',
+		'module_mode'      => 'customblock',
+		'module_auth'      => ''
+	),
+	'linkblock' => array(
+		'module_basename'   => 'portal',
+		'module_enabled'   => 1,
+		'module_display'   => 1,
+		'module_class'      => 'acp',
+		'module_langname'   => 'ACP_PORTAL_LINKS_INFO',
+		'module_mode'      => 'links',
+		'module_auth'      => ''
+	),
+	);
 
 ?>
