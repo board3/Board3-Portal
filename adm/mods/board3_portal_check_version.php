f003470cbc7cab60204abce5d421c655251eb2b7
Kevin <kevin@breest.info>
2008-08-10 14:34:55 +0000

    Added "MOD Version Check" support

---
+++ adm/mods/board3_portal_check_version.php
@@ -0,0 +1,38 @@
+<?php
+/**
+*
+* @package acp
+* @version $Id: mod_version_check_version.php
+* @copyright (c) 2007 StarTrekGuide
+* @license http://opensource.org/licenses/gpl-license.php GNU Public License
+*
+*/
+
+/**
+* @package mod_version_check
+*/
+
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+class board3_portal_check_version
+{
+	function version()
+	{
+		global $portal_config, $phpbb_root_path, $phpEx;
+			include($phpbb_root_path . 'portal/includes/functions.' . $phpEx);
+			$portal_config = obtain_portal_config();
+
+		return array(
+			'author'	=> 'Saint_hh',
+			'title'		=> 'Board3 Portal',
+			'tag'		=> 'board3_portal',
+			'version'	=> $portal_config['portal_version'],
+			'file'		=> array('board3.de', 'updatecheck', 'board3_portal.xml'),
+		);
+	}
+}
+
+?>
