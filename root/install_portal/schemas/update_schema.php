d127fc3b9c1a9cfdb595ec9da7487fd2eb10fb32
Kevin <kevin@breest.info>
2008-08-17 18:59:40 +0000

    Add Chris to the header, promised that a while ago. ;)

--- root/install_portal/schemas/update_schema.php
+++ root/install_portal/schemas/update_schema.php
@@ -1,5 +1,15 @@
 <?php
 
+/**
+*
+* @package - Board3portal
+* @version $Id$
+* @copyright (c) kevin / saint ( www.board3.de/ ), (c) Ice, (c) nickvergessen ( www.flying-bits.org/ ), (c) redbull254 ( www.digitalfotografie-foren.de ), (c) Christian_N ( www.phpbb-projekt.de )
+* @based on: phpBB3 Portal by Sevdin Filiz, www.phpbb3portal.com
+* @license http://opensource.org/licenses/gpl-license.php GNU Public License 
+*
+*/
+
 $sql_update['0.2.0'] = array(
 //	"ALTER TABLE phpbb_portal_config CHANGE config_value config_value text NOT NULL",
 	"INSERT INTO phpbb_portal_config (config_name, config_value) VALUES ('portal_poll_limit', '3')",
