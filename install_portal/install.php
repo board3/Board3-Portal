22b36b91a7ab8e43420f198ed58b2adf74d675d5
Kevin <kevin@breest.info>
2008-08-17 18:59:40 +0000

    Add Chris to the header, promised that a while ago. ;)

--- install_portal/install.php
+++ install_portal/install.php
@@ -1,5 +1,15 @@
 <?php
 
+/**
+*
+* @package - Board3portal
+* @version $Id$
+* @copyright (c) kevin / saint ( www.board3.de/ ), (c) Ice, (c) nickvergessen ( www.flying-bits.org/ ), (c) redbull254 ( www.digitalfotografie-foren.de ), (c) Christian_N ( www.phpbb-projekt.de )
+* @based on: phpBB3 Portal by Sevdin Filiz, www.phpbb3portal.com
+* @license http://opensource.org/licenses/gpl-license.php GNU Public License 
+*
+*/
+
 $current_version = '1.0.0RC2';
 
 // If only checking version, exit.
--- portal/includes/functions.php
+++ portal/includes/functions.php
@@ -4,7 +4,7 @@
 *
 * @package - Board3portal
 * @version $Id$
-* @copyright (c) kevin / saint ( http://www.board3.de/ ), (c) Ice, (c) nickvergessen ( http://www.flying-bits.org/ ), (c) redbull254 ( http://www.digitalfotografie-foren.de )
+* @copyright (c) kevin / saint ( www.board3.de/ ), (c) Ice, (c) nickvergessen ( www.flying-bits.org/ ), (c) redbull254 ( www.digitalfotografie-foren.de ), (c) Christian_N ( www.phpbb-projekt.de )
 * @based on: phpBB3 Portal by Sevdin Filiz, www.phpbb3portal.com
 * @license http://opensource.org/licenses/gpl-license.php GNU Public License 
 *
