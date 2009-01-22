b5a95e65d0e7e0012f7ba98fad1454ade98baba0
Christian <christian_n84@yahoo.de>
2009-01-22 01:56:19 +0000

    Edited the language-files, thanks Thomas.

--- root/language/de/mods/install_portal.php
+++ root/language/de/mods/install_portal.php
@@ -23,41 +23,40 @@ if (empty($lang) || !is_array($lang))
 }
 
 $lang = array_merge($lang, array(
-	'INSTALL_CONGRATS_EXPLAIN'		=> '<p>Du hast das board3 Portal v%s nun erfolgreich installiert.<br/><br/><strong>Bitte lösche oder verschiebe nun das Installations-Verzeichnis „install“ oder nenne es nun um, bevor du dein Board benutzt. Solange dieses Verzeichnis existiert, ist nur der Administrations-Bereich zugänglich.</strong></p>',
-	'INSTALL_INTRO_BODY'			=> 'Dieser Assistent ermöglicht dir die Installation des board3 Portal in deinem phpBB-Board.',
+	'INSTALL_CONGRATS_EXPLAIN'		=> '<p>Du hast das Board3 Portal v%s erfolgreich installiert.<br/><br/><strong>Bitte lösche oder verschiebe jetzt das Installations-Verzeichnis "install" oder benenne es um, bevor du dein Board benutzt. Solange dieses Verzeichnis existiert, ist nur der Administrations-Bereich zugänglich.</strong></p>',
+	'INSTALL_INTRO_BODY'			=> 'Dieser Assistent unterstützt dich bei der Installation des Board3 Portals in deinem phpBB-Forum.',
 
-	'MISSING_CONSTANTS'				=> 'Bevor du das Installations-Skript aufrufen kannst, musst du deine geänderten Dateien hochladen, insbesondere die includes/constants.php.',
+	'MISSING_CONSTANTS'				=> 'Bevor du das Installations-Skript aufrufen kannst, musst du die bearbeiteten Dateien hochladen, insbesondere /includes/constants.php.',
 	'MODULES_CREATE_PARENT'			=> 'Übergeordnetes Standard-Modul erstellen',
 	'MODULES_PARENT_SELECT'			=> 'Übergeordnetes Modul auswählen',
 	'MODULES_SELECT_4ACP'			=> 'Übergeordnetes Modul für den "Administrations-Bereich"',
 	'MODULES_SELECT_NONE'			=> 'kein übergeordnetes Modul',
 
-	'STAGE_ADVANCED_EXPLAIN'		=> 'Bitte wähle die übergeordneten Module für die Module des board3 Portal aus. Im Normalfall solltest du diese Einstellungen nicht verändern.',
-	'STAGE_CREATE_TABLE_EXPLAIN'	=> 'Die von des board3 Portal genutzten Datenbank-Tabellen wurden nun erstellt und mit einigen Ausgangswerten gefüllt. Geh weiter zum nächsten Schritt, um die Installation des board3 Portal abzuschließen.',
-	'STAGE_ADVANCED_SUCCESSFUL'		=> 'Die von des board3 Portal genutzten Module wurden nun erstellt. Geh weiter um die Installation des board3 Portal abzuschließen.',
+	'STAGE_ADVANCED_EXPLAIN'		=> 'Bitte wähle die übergeordneten Module für die Module des Board3 Portals aus. Im Normalfall solltest du diese Einstellungen nicht verändern.',
+	'STAGE_CREATE_TABLE_EXPLAIN'	=> 'Die vom Board3 Portal genutzten Datenbank-Tabellen wurden erstellt und mit einigen Ausgangswerten gefüllt. Gehe weiter zum nächsten Schritt, um die Installation des Board3 Portals abzuschließen.',
+	'STAGE_ADVANCED_SUCCESSFUL'		=> 'Die vom Board3 Portal genutzten Module wurden erstellt. Gehe weiter um die Installation des Board3 Portals abzuschließen.',
 	'STAGE_UNINSTALL'				=> 'Deinstallieren',
-	
+    
 	'FILES_EXISTS'					=> 'Datei existiert noch',
 	'FILES_OUTDATED'				=> 'Veraltete Dateien',
-	'FILES_OUTDATED_EXPLAIN'		=> '<strong>Veraltete Dateien</strong> - bitte entferne die folgenden Dateien um mögliche Sicherheitslücken zu entfernen.',
-	'REQUIREMENTS_EXPLAIN'			=> 'Bitte entferne erst alle veraltete Dateien auf dein Webspace, bevor mit dem Update fortfährst.',
-	'NOT_REQUIREMENTS_EXPLAIN'		=> 'Es wurden keine veraltete Dateien auf dein Webspace gefunden, du kannst nun mit dem Update fortfahren.',
+	'FILES_OUTDATED_EXPLAIN'		=> '<strong>Veraltete Dateien</strong> - bitte entferne die folgenden Dateien, um eventuelle Sicherheitslücken zu schließen.',
+	'REQUIREMENTS_EXPLAIN'			=> 'Bitte entferne erst alle veralteten Dateien von deinem Server, bevor mit dem Update fortfährst.',
+	'NOT_REQUIREMENTS_EXPLAIN'		=> 'Es wurden keine veraltete Dateien auf deinem Server gefunden, du kannst mit dem Update fortfahren.',
 
-	'UPDATE_INSTALLATION'			=> 'board3 Portal aktualisieren',
-	'UPDATE_INSTALLATION_EXPLAIN'	=> 'Mit dieser Option kannst du deine board3 Portal-Version auf den neuesten Stand bringen.',
-	'UPDATE_CONGRATS_EXPLAIN'		=> '<p>Du hast das board3 Portal nun erfolgreich auf v%s aktualisiert.<br/><br/><strong>Bitte lösche oder verschiebe nun das Installations-Verzeichnis „install“ oder nenne es nun um, bevor du dein Board benutzt. Solange dieses Verzeichnis existiert, ist nur der Administrations-Bereich zugänglich.</strong></p>',
-	
+	'UPDATE_INSTALLATION'			=> 'Board3 Portal aktualisieren',
+	'UPDATE_INSTALLATION_EXPLAIN' 	=> 'Mit dieser Option kannst du dein Board3 Portal auf den aktuellen Versionsstand bringen.',
+	'UPDATE_CONGRATS_EXPLAIN'		=> '<p>Du hast das Board3 Portal erfolgreich auf v%s aktualisiert.<br/><br/><strong>Bitte lösche oder verschiebe jetzt das Installations-Verzeichnis "install" oder benenne es um, bevor du dein Board benutzt. Solange dieses Verzeichnis existiert, ist nur der Administrations-Bereich zugänglich.</strong></p>',
+    
 	'UNINSTALL_INTRO'				=> 'Willkommen bei der Deinstallation',
-	'UNINSTALL_INTRO_BODY'			=> 'Dieser Assistent ermöglicht dir die deinstallation des board3 Portal durchzuführen.',
+	'UNINSTALL_INTRO_BODY'			=> 'Dieser Assistent unterstützt dich bei der De-Installation des Board3 Portals.',
 	'CAT_UNINSTALL'					=> 'Deinstallieren',
 	'UNINSTALL_CONGRATS'			=> '<h1>Board3 Portal deinstalliert.</h1>
-										Du hast das board3 Portal nun erfolgreich deinstalliert.',
-	'UNINSTALL_CONGRATS_EXPLAIN'	=> '<strong>Bitte lösche oder verschiebe nun das Installations-Verzeichnis „install“ oder nenne es nun um, bevor du dein Board benutzt. Solange dieses Verzeichnis existiert, ist nur der Administrations-Bereich zugänglich.<br /><br />Denke daran die Portal-Dateien zu löschen und Dateiänderungen am Originalsystem rückgängig zu machen.</strong></p>',
+                                        Du hast das Board3 Portal erfolgreich deinstalliert.',
+	'UNINSTALL_CONGRATS_EXPLAIN'	=> '<strong>Bitte lösche oder verschiebe jetzt das Installations-Verzeichnis "install" oder benenne es um, bevor du dein Board benutzt. Solange dieses Verzeichnis existiert, ist nur der Administrations-Bereich zugänglich.<br /><br />Denke daran die Portal-Dateien zu löschen und Dateiänderungen am Originalsystem rückgängig zu machen.</strong></p>',
 
-	'SUPPORT_BODY'					=> 'Für die aktuelle, stabile Version des "board3 Portal" wird kostenloser Support gewährt. Dies umfasst:</p><ul><li>Installation</li><li>Technische Fragen</li><li>Probleme durch eventuelle Fehler in der Software</li><li>Aktualisierung von Release Candidates (RC) oder stabilen Versionen zur aktuellen stabilen Version</li></ul><p>Support gibt es in folgenden Foren:</p><ul><li><a href="http://www.board3.de/">board3.de - Homepage des MOD-Autor\'s Kevin</a></li><li><a href="http://www.phpbb.de/">phpbb.de</a></li><li><a href="http://www.phpbb.com/">phpbb.com</a></li></ul><p>',
+	'SUPPORT_BODY'					=> 'Für die aktuelle, stabile Version des "Board3 Portal" wird kostenloser Support gewährt. Dieser umfasst:</p><ul><li>Installation</li><li>Technische Fragen</li><li>Probleme durch eventuelle Fehler in der Software</li><li>Aktualisierung von Release Candidates (RC) oder stabilen Versionen zur aktuellen stabilen Version</li></ul><p>Support gibt es in folgenden Foren:</p><ul><li><a href="http://www.board3.de/">board3.de - Homepage des MOD-Autor\'s Kevin</a></li><li><a href="http://www.phpbb.de/">phpbb.de</a></li><li><a href="http://www.phpbb.com/">phpbb.com</a></li></ul><p>',
 	'GOTO_INDEX'					=> 'Gehe zum Forum',
 	'GOTO_PORTAL'					=> 'Gehe zum Portal',
-
 ));
 
 ?>
--- root/language/en/mods/install_portal.php
+++ root/language/en/mods/install_portal.php
@@ -23,41 +23,40 @@ if (empty($lang) || !is_array($lang))
 }
 
 $lang = array_merge($lang, array(
-	'INSTALL_CONGRATS_EXPLAIN'		=> '<p>Du hast das board3 Portal v%s nun erfolgreich installiert.<br/><br/><strong>Bitte lösche oder verschiebe nun das Installations-Verzeichnis „install“ oder nenne es nun um, bevor du dein Board benutzt. Solange dieses Verzeichnis existiert, ist nur der Administrations-Bereich zugänglich.</strong></p>',
-	'INSTALL_INTRO_BODY'			=> 'Dieser Assistent ermöglicht dir die Installation des board3 Portal in deinem phpBB-Board.',
+	'INSTALL_CONGRATS_EXPLAIN'		=> 	'<p>You have succesfully installed the Board3 Portal v%s<br/><br/><strong>Now delete, move or rename the "install"-folder before you use your board. As long as this directory is present, you will only have access to your ACP.</strong></p>',
+	'INSTALL_INTRO_BODY'			=> 	'This installation system will guide you through installing the Board3 Portal to your phpBB forum.',
 
-	'MISSING_CONSTANTS'				=> 'Bevor du das Installations-Skript aufrufen kannst, musst du deine geänderten Dateien hochladen, insbesondere die includes/constants.php.',
-	'MODULES_CREATE_PARENT'			=> 'Übergeordnetes Standard-Modul erstellen',
-	'MODULES_PARENT_SELECT'			=> 'Übergeordnetes Modul auswählen',
-	'MODULES_SELECT_4ACP'			=> 'Übergeordnetes Modul für den "Administrations-Bereich"',
-	'MODULES_SELECT_NONE'			=> 'kein übergeordnetes Modul',
+	'MISSING_CONSTANTS'			=> 	'Prior to run the install script, you have to upload the edited files, especially /includes/constants.php.',
+	'MODULES_CREATE_PARENT'		=> 	'Create parent standard module',
+	'MODULES_PARENT_SELECT'		=> 	'Select parent module',
+	'MODULES_SELECT_4ACP'		=> 	'Parent module for the ACP',
+	'MODULES_SELECT_NONE'		=> 	'No parent module',
 
-	'STAGE_ADVANCED_EXPLAIN'		=> 'Bitte wähle die übergeordneten Module für die Module des board3 Portal aus. Im Normalfall solltest du diese Einstellungen nicht verändern.',
-	'STAGE_CREATE_TABLE_EXPLAIN'	=> 'Die von des board3 Portal genutzten Datenbank-Tabellen wurden nun erstellt und mit einigen Ausgangswerten gefüllt. Geh weiter zum nächsten Schritt, um die Installation des board3 Portal abzuschließen.',
-	'STAGE_ADVANCED_SUCCESSFUL'		=> 'Die von des board3 Portal genutzten Module wurden nun erstellt. Geh weiter um die Installation des board3 Portal abzuschließen.',
-	'STAGE_UNINSTALL'				=> 'Deinstallieren',
-	
-	'FILES_EXISTS'					=> 'Datei existiert noch',
-	'FILES_OUTDATED'				=> 'Veraltete Dateien',
-	'FILES_OUTDATED_EXPLAIN'		=> '<strong>Veraltete Dateien</strong> - bitte entferne die folgenden Dateien um mögliche Sicherheitslücken zu entfernen.',
-	'REQUIREMENTS_EXPLAIN'			=> 'Bitte entferne erst alle veraltete Dateien auf dein Webspace, bevor mit dem Update fortfährst.',
-	'NOT_REQUIREMENTS_EXPLAIN'		=> 'Es wurden keine veraltete Dateien auf dein Webspace gefunden, du kannst nun mit dem Update fortfahren.',
+	'STAGE_ADVANCED_EXPLAIN'		=> 	'Please select the parent modules for the Board3 Portal modules. Usually there is no need to change these settings.',
+	'STAGE_CREATE_TABLE_EXPLAIN'	=> 	'The Board3 Portal database tables have been created and initialized with basic values. Proceed to the next step to finish the Board3 Portal installation.',
+	'STAGE_ADVANCED_SUCCESSFUL'		=> 	'The Board3 Portal modules have been created. Proceed to finish the Board3 Portal installation.',
+	'STAGE_UNINSTALL'				=> 	'Uninstall',
 
-	'UPDATE_INSTALLATION'			=> 'board3 Portal aktualisieren',
-	'UPDATE_INSTALLATION_EXPLAIN'	=> 'Mit dieser Option kannst du deine board3 Portal-Version auf den neuesten Stand bringen.',
-	'UPDATE_CONGRATS_EXPLAIN'		=> '<p>Du hast das board3 Portal nun erfolgreich auf v%s aktualisiert.<br/><br/><strong>Bitte lösche oder verschiebe nun das Installations-Verzeichnis „install“ oder nenne es nun um, bevor du dein Board benutzt. Solange dieses Verzeichnis existiert, ist nur der Administrations-Bereich zugänglich.</strong></p>',
-	
-	'UNINSTALL_INTRO'				=> 'Willkommen bei der Deinstallation',
-	'UNINSTALL_INTRO_BODY'			=> 'Dieser Assistent ermöglicht dir die deinstallation des board3 Portal durchzuführen.',
-	'CAT_UNINSTALL'					=> 'Deinstallieren',
-	'UNINSTALL_CONGRATS'			=> '<h1>Board3 Portal deinstalliert.</h1>
-										Du hast das board3 Portal nun erfolgreich deinstalliert.',
-	'UNINSTALL_CONGRATS_EXPLAIN'	=> '<strong>Bitte lösche oder verschiebe nun das Installations-Verzeichnis „install“ oder nenne es nun um, bevor du dein Board benutzt. Solange dieses Verzeichnis existiert, ist nur der Administrations-Bereich zugänglich.<br /><br />Denke daran die Portal-Dateien zu löschen und Dateiänderungen am Originalsystem rückgängig zu machen.</strong></p>',
+	'FILES_EXISTS'				=> 	'File still exists',
+	'FILES_OUTDATED'			=> 	'Out-of-date files',
+	'FILES_OUTDATED_EXPLAIN'	=> 	'<strong>Out-of-date files</strong> - please delete these files to avoid security issues.',
+	'REQUIREMENTS_EXPLAIN'		=> 	'Please delete all out-of-date files from your server prior to proceed with updating.',
+	'NOT_REQUIREMENTS_EXPLAIN'	=> 	'No out-of-date files were found on your server, you may proceed with updating.',
 
-	'SUPPORT_BODY'					=> 'Für die aktuelle, stabile Version des "board3 Portal" wird kostenloser Support gewährt. Dies umfasst:</p><ul><li>Installation</li><li>Technische Fragen</li><li>Probleme durch eventuelle Fehler in der Software</li><li>Aktualisierung von Release Candidates (RC) oder stabilen Versionen zur aktuellen stabilen Version</li></ul><p>Support gibt es in folgenden Foren:</p><ul><li><a href="http://www.board3.de/">board3.de - Homepage des MOD-Autor\'s Kevin</a></li><li><a href="http://www.phpbb.de/">phpbb.de</a></li><li><a href="http://www.phpbb.com/">phpbb.com</a></li></ul><p>',
-	'GOTO_INDEX'					=> 'Gehe zum Forum',
-	'GOTO_PORTAL'					=> 'Gehe zum Portal',
+	'UPDATE_INSTALLATION'			=> 	'Update Board3 Portal',
+	'UPDATE_INSTALLATION_EXPLAIN'	=> 	'This option will update your Board3 Portal to the current version.',
+	'UPDATE_CONGRATS_EXPLAIN'		=> 	'<p>You have updated your Board3 Portal successfully to v%s<br/><br/><strong>Now delete, move or rename the "install"-folder before you use your board. As long as this directory is present, you will only have access to your ACP.</strong></p>',
 
+	'UNINSTALL_INTRO'				=> 	'Welcome to Uninstall',
+	'UNINSTALL_INTRO_BODY'			=> 	'This installation system will guide you through uninstalling the Board3 Portal from your phpBB forum.',
+	'CAT_UNINSTALL'					=> 	'Uninstall',
+	'UNINSTALL_CONGRATS'			=> 	'<h1>Board3 Portal removed.</h1>
+									You have successfully uninstalled the Board3 Portal.',
+	'UNINSTALL_CONGRATS_EXPLAIN'	=> 	'<strong>Now delete, move or rename the "install"-folder before you use your board. As long as this directory is present, you will only have access to your ACP.<br /><br />Make sure to delete the Portal-related files and reverse all Portal-related edits of phpBB core files.</strong></p>',
+
+	'SUPPORT_BODY'		=> 	'Support for the latest stable version of the Board3 Portal is available free of charge for:</p><ul><li>Installation</li><li>Technical questions</li><li>Program-related issues</li><li>Updating Release Candidates (RC) or stable versions to the latest stable version</li></ul><p>You will find support in these forums:</p><ul><li><a href="http://www.board3.de/">board3.de - Homepage of Kevin - MOD author</a></li><li><a href="http://www.phpbb.de/">phpbb.de</a></li><li><a href="http://www.phpbb.com/">phpbb.com</a></li></ul><p>',
+	'GOTO_INDEX'		=> 	'Proceed to forum',
+	'GOTO_PORTAL'		=> 	'Proceed to Portal',
 ));
 
 ?>
