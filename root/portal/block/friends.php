66c641770ce8d4e2aa6a0c9f044fb01baa667298
Christian <christian_n84@yahoo.de>
2009-08-27 12:24:51 +0000

    SQL ERROR [ postgres ]
    ERROR: for SELECT DISTINCT, ORDER BY expressions must appear in select list []
    
    http://www.board3.de/viewtopic.php?f=10&t=2587&p=17094&hilit=postgres#p17094
    
    Patch by NV

--- root/portal/block/friends.php
+++ root/portal/block/friends.php
@@ -25,55 +25,26 @@ $s_display_friends = false;
 // Output listing of friends online
 $update_time = $config['load_online_time'] * 60;
 
-	switch ($db->sql_layer)
-	{
-		case 'mssql':
-		case 'mssql_odbc':
-			$sql = $db->sql_build_query('SELECT_DISTINCT', array(
-				'SELECT'	=> 'u.user_id, u.username, u.user_colour, u.user_allow_viewonline, MAX(s.session_time) as online_time, MIN(s.session_viewonline) AS viewonline',
-				'FROM'		=> array(
-										USERS_TABLE	=> 'u',
-										ZEBRA_TABLE	=> 'z'
-								),
+$sql = $db->sql_build_query('SELECT_DISTINCT', array(
+	'SELECT'	=> 'u.user_id, u.username, u.username_clean, u.user_colour, u.user_allow_viewonline, MAX(s.session_time) as online_time, MIN(s.session_viewonline) AS viewonline',
+	'FROM'		=> array(
+		USERS_TABLE	=> 'u',
+		ZEBRA_TABLE	=> 'z'
+	),
 
-				'LEFT_JOIN'	=> array(
-					array(
-				'FROM'	=> array(SESSIONS_TABLE => 's'),
-				'ON'	=> 's.session_user_id = z.zebra_id'
-					)
-				),
+	'LEFT_JOIN'	=> array(
+		array(
+			'FROM'	=> array(SESSIONS_TABLE => 's'),
+			'ON'	=> 's.session_user_id = z.zebra_id'
+		)
+	),
 
-				'WHERE'		=> 'z.user_id = ' . $user->data['user_id'] . '
-				AND z.friend = 1
-				AND u.user_id = z.zebra_id',
-				'GROUP_BY'	=> 'z.zebra_id, u.user_id, u.username, u.user_allow_viewonline, u.user_colour',
-				'ORDER_BY'   => 'u.username ASC',
-			));
-		break;
-	
-		default:
-			$sql = $db->sql_build_query('SELECT_DISTINCT', array(
-				'SELECT'	=> 'u.user_id, u.username, u.user_colour, u.user_allow_viewonline, MAX(s.session_time) as online_time, MIN(s.session_viewonline) AS viewonline',
-				'FROM'		=> array(
-										USERS_TABLE	=> 'u',
-										ZEBRA_TABLE	=> 'z'
-								),
-
-				'LEFT_JOIN'	=> array(
-					array(
-				'FROM'	=> array(SESSIONS_TABLE => 's'),
-				'ON'	=> 's.session_user_id = z.zebra_id'
-					)
-				),
-
-				'WHERE'		=> 'z.user_id = ' . $user->data['user_id'] . '
-				AND z.friend = 1
-				AND u.user_id = z.zebra_id',
-				'GROUP_BY'	=> 'z.zebra_id, u.user_id, u.username, u.user_allow_viewonline, u.user_colour',
-				'ORDER_BY'	=> 'u.username_clean ASC',
-			));
-		break;
-	}
+	'WHERE'		=> 'z.user_id = ' . $user->data['user_id'] . '
+		AND z.friend = 1
+		AND u.user_id = z.zebra_id',
+	'GROUP_BY'	=> 'z.zebra_id, u.user_id, u.username, u.user_allow_viewonline, u.user_colour',
+	'ORDER_BY'   => 'u.username_clean ASC',
+));
 
 $result = $db->sql_query_limit($sql, $portal_config['portal_max_online_friends']);
 
