0e4047af59e3728b492f6b4b2f8fefd8dde35b51
Christian <christian_n84@yahoo.de>
2009-08-28 13:01:35 +0000

    Versioncheck from board3.de :-)

--- root/adm/mods/board3_portal_check_version.php
+++ root/adm/mods/board3_portal_check_version.php
@@ -26,14 +26,14 @@ class board3_portal_check_version
 		{
 			include($phpbb_root_path . 'portal/includes/functions.' . $phpEx);
 		}
-		$portal_config = obtain_portal_config();	
+		$portal_config = obtain_portal_config();
 
 		return array(
 			'author'	=> 'Saint_hh',
 			'title'		=> 'Board3 Portal',
-			'tag'		=> 'b3p_v1_modcheck',
+			'tag'		=> 'board3_portal',
 			'version'	=> $portal_config['portal_version'],
-			'file'		=> array('phpbb-projekt.de', 'updatecheck', 'b3p.xml'),
+			'file'		=> array('board3.de', 'updatecheck', 'board3_portal.xml'),
 		);
 	}
 }
