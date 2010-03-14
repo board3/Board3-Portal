1963fef4c22603a11263131e5ec66f0557d7b3a9
Marc Alexander <admin@m-a-styles.de>
2010-03-14 15:58:41 +0000

    Wordgraph is disabled if any search type other than fulltext native is chosen in the ACP

--- root/portal/block/wordgraph.php
+++ root/portal/block/wordgraph.php
@@ -16,54 +16,66 @@ if (!defined('IN_PHPBB') || !defined('IN_PORTAL'))
 }
 
 $words_array = array();
+$display_wordgraph = true;
 
-// Get words and number of those words
-$sql = 'SELECT word_text, word_count, word_id
-    FROM ' . SEARCH_WORDLIST_TABLE . '
-    GROUP BY word_id, word_text 
-    ORDER BY word_count DESC'; 
-$result = $db->sql_query_limit($sql, $portal_config['portal_wordgraph_max_words']);
-
-while ($row = $db->sql_fetchrow($result))
+/**
+* wordgraph is not available when we do not use the fulltext native search
+*/
+if($config['search_type'] == 'fulltext_native')
 {
-	$word = strtolower($row['word_text']);
-	$words_array[$word] = $row['word_count'];
-}
-$db->sql_freeresult($result);
+	
+	// Get words and number of those words
+	$sql = 'SELECT word_text, word_count, word_id
+		FROM ' . SEARCH_WORDLIST_TABLE . '
+		GROUP BY word_id, word_text 
+		ORDER BY word_count DESC'; 
+	$result = $db->sql_query_limit($sql, $portal_config['portal_wordgraph_max_words']);
 
-$minimum = 1000000;
-$maximum = -1000000;
-
-foreach (array_keys($words_array) as $word)
-{
-	if ($words_array[$word] > $maximum)
+	while ($row = $db->sql_fetchrow($result))
 	{
-		$maximum = $words_array[$word];
+		$word = strtolower($row['word_text']);
+		$words_array[$word] = $row['word_count'];
+	}
+	$db->sql_freeresult($result);
+
+	$minimum = 1000000;
+	$maximum = -1000000;
+
+	foreach (array_keys($words_array) as $word)
+	{
+		if ($words_array[$word] > $maximum)
+		{
+			$maximum = $words_array[$word];
+		}
+
+		if ($words_array[$word] < $minimum)
+		{
+			$minimum = $words_array[$word];
+		}
 	}
 
-	if ($words_array[$word] < $minimum)
+	// ratio
+	$ratio = $portal_config['portal_wordgraph_ratio'] / ($maximum - $minimum +1);
+
+	$words = array_keys($words_array);
+	sort($words);
+
+	foreach ($words as $word)
 	{
-		$minimum = $words_array[$word];
+		$template->assign_block_vars('wordgraph', array(
+			'WORD'				=> ($portal_config['portal_wordgraph_word_counts']) ? $word . '(' . $words_array[$word] . ')' : $word,
+			'WORD_FONT_SIZE'	=> (int) (9 + ($words_array[$word] * $ratio)),
+			'WORD_SEARCH_URL'	=> append_sid("{$phpbb_root_path}search.$phpEx", 'keywords=' . urlencode($word)),
+		));
 	}
 }
-
-// ratio
-$ratio = $portal_config['portal_wordgraph_ratio'] / ($maximum - $minimum +1);
-
-$words = array_keys($words_array);
-sort($words);
-
-foreach ($words as $word)
+else
 {
-	$template->assign_block_vars('wordgraph', array(
-		'WORD'				=> ($portal_config['portal_wordgraph_word_counts']) ? $word . '(' . $words_array[$word] . ')' : $word,
-		'WORD_FONT_SIZE'	=> (int) (9 + ($words_array[$word] * $ratio)),
-		'WORD_SEARCH_URL'	=> append_sid("{$phpbb_root_path}search.$phpEx", 'keywords=' . urlencode($word)),
-	));
+	$display_wordgraph = false;
 }
 
 $template->assign_vars(array(
-	'S_DISPLAY_WORDGRAPH'	=> true,
+	'S_DISPLAY_WORDGRAPH'	=> ($display_wordgraph) ? true : false,
 	'L_WORDGRAPH'			=> $user->lang['WORDGRAPH'],
 	)
 );
