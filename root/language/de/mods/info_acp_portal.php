3c4285a50423966f13d79a76da33ff6cf5315ab8
Marc Alexander <admin@m-a-styles.de>
2010-11-27 01:10:43 +0000

    Added basic set of German language - still missing module's language files in German

--- root/language/de/mods/info_acp_portal.php
+++ root/language/de/mods/info_acp_portal.php
@@ -35,122 +35,60 @@ if (empty($lang) || !is_array($lang))
 
 
 $lang = array_merge($lang, array(
-	// Manage blocks
-	'ACP_PORTAL_BLOCKS'				=> 'Block-Verwaltung',
-	'ACP_PORTAL_BLOCKS_EXPLAIN'		=> 'Du kannst hier Blöcke anlegen, bearbeiten und löschen.',
-	'ADD_BLOCK'						=> 'Neuer Block hinzufügen',
-	'ACP_PORTAL_MANAGE_BLOCKS'		=> 'Blöcke verwalten',
+	// Portal Module
+	'ACP_PORTAL_MODULES'			=> 'Portal Module',
+	'ACP_PORTAL_MODULES_EXP'		=> 'Du kannst deine Portal Module hier verwalten. Falls du alle Module deaktivierst, dann deaktiviere bitte auch das Portal.',
+	
+	'MODULE_POS_TOP'				=> 'Oben',
+	'MODULE_POS_LEFT'				=> 'Linke Spalte',
+	'MODULE_POS_RIGHT'				=> 'Rechte Spalte',
+	'MODULE_POS_CENTER'				=> 'Mittlere Spalte',
+	'MODULE_POS_BOTTOM'				=> 'Unten',
+	'ADD_MODULE'					=> 'Modul Hinzufügen',
+	'CHOOSE_MODULE'					=> 'Modul Auswählen',
+	'CHOOSE_MODULE_EXP'				=> 'Wähle ein Modul von der Dropdown-Liste',
+	'SUCCESS_ADD'					=> 'Das Modul wurde erfolgreich hinzugefügt.',
+	'SUCCESS_DELETE'				=> 'Das Modul wurde erfolgreich entfernt.',
+	
+	'MODULE_OPTIONS'			=> 'Modul Optionen',
+	'MODULE_NAME'				=> 'Modul Name',
+	'MODULE_NAME_EXP'			=> 'Gebe den Namen ein der für das Modul in der Modul Konfiguration angezeigt werden soll.',
+	'MODULE_IMAGE'				=> 'Modul Bild',
+	'MODULE_IMAGE_EXP'			=> 'Gebe den Dateinamen des Modul Bildes ein. Bilder müssen sich in styles/*yourstyle*/theme/images/portal/ befinden.',
+	'MODULE_PERMISSIONS'		=> 'Modul Berechtigungen',
+	'MODULE_PERMISSIONS_EXP'	=> 'Select the groups that should be authorized to view the module. If you want all users to be able to view the module, don’t select anything.<br />Select/Deselect multiple groups by holding <samp>CTRL</samp> and clicking.',
 
 	// general
 	'ACP_PORTAL'							=> 'Portal',
 	'ACP_PORTAL_GENERAL_INFO'				=> 'Allgemeine Einstellungen',
-	'ACP_PORTAL_GENERAL_TITLE'				=> 'Portal Administration',
-	'ACP_PORTAL_GENERAL_TITLE_EXPLAIN'		=> 'Hier kannst du allgemeine Einstellungen vornehmen.',
+	'ACP_PORTAL_CONFIG_INFO'				=> 'Allgemeine Einstellungen',
+	'ACP_PORTAL_GENERAL_TITLE'				=> 'Portal Verwaltung',
+	'ACP_PORTAL_GENERAL_TITLE_EXP'			=> 'Danke, dass du dich für board3 Portal entschieden hast. Auf dieser Seite kannst du dein Portal verwalten. Diese Anzeige gibt dir einen schnellen Überblick über die verschiedenen Portal-Einstellungen.',
+	'PORTAL_ENABLE'							=> 'Portal aktivieren',
+	'PORTAL_ENABLE_EXP'						=> 'Wenn deaktiviert, wird das komplette Portal abgeschaltet.',
+	'PORTAL_LEFT_COLUMN'					=> 'Linke Spalte aktivieren',
+	'PORTAL_LEFT_COLUMN_EXP'				=> 'Die Linke Spalte auf dem Portal anzeigen',
+	'PORTAL_RIGHT_COLUMN'					=> 'Rechte Spalte aktivieren',
+	'PORTAL_RIGHT_COLUMN_EXP'				=> 'Die Rechte Spalte auf dem Portal anzeigen',
+	'PORTAL_VERSION_CHECK'					=> 'Versioncheck auf Portal',
+	'PORTAL_FORUM_INDEX'					=> 'Foren Index (Foren Liste)',
+	'PORTAL_FORUM_INDEX_EXP'				=> 'Diesen Block auf dem Portal anzeigen.',
+	'ACP_PORTAL_COLUMN_WIDTH_SETTINGS'		=> 'Breiteneinstellung der rechten und linken Spalte',
+	'PORTAL_LEFT_COLUMN_WIDTH'				=> 'Breite der linken Spalte',
+	'PORTAL_LEFT_COLUMN_WIDTH_EXP'			=> 'Ändere hier die Breite der linken Spalte in Pixel, empfohlener Wert 180',
+	'PORTAL_RIGHT_COLUMN_WIDTH'				=> 'Breite der rechten Spalte',
+	'PORTAL_RIGHT_COLUMN_WIDTH_EXP'			=> 'Ändere hier die Breite der rechten Spalte in Pixel, empfohlener Wert 180',
 	
-	'BLOCK_ADDED'					=> 'The block was successfully added.',
-	'BLOCK_FILENAME'				=> 'Block template file',
-	'BLOCK_FILENAME_EXPLAIN'		=> 'Use this to define a tempalte file used for the block. The path is relative to the root phpBB directory.',
-	'BLOCK_ICON'					=> 'Block icon',
-	'BLOCK_ICON_EXPLAIN'			=> 'Use this to define a small icon associated with the block. The path is relative to the root phpBB directory.',
-	'BLOCK_POSITION'				=> 'Display position',
-	'BLOCK_POSITION_EXPLAIN'		=> 'Select the display position on index page.',
-	'BLOCK_POSITION_BOTTOM'			=> 'Bottom',
-	'BLOCK_POSITION_LEFT'			=> 'Left',
-	'BLOCK_POSITION_MIDDLE_BOTTOM'	=> 'Middle - Bottom',
-	'BLOCK_POSITION_MIDDLE_TOP'		=> 'Middle - Top',
-	'BLOCK_POSITION_NONE'			=> 'Not display',
-	'BLOCK_POSITION_RIGHT'			=> 'Right',
-	'BLOCK_POSITION_TOP'			=> 'Top',
-	'BLOCK_REMOVED'					=> 'The block was successfully deleted.',
-	'BLOCK_TEXT'					=> 'Text',
-	'BLOCK_TEXT_EXPLAIN'			=> 'Enter here the content of the block text.',
-	'BLOCK_TITLE'					=> 'Block title',
-	'BLOCK_TITLE_EXPLAIN'			=> 'Enter the displayed block title. Use language constant if name is served from language file: <samp>language/en/mods/portal.php</samp>',
-	'BLOCK_TYPE'					=> 'Block type',
-	'BLOCK_TYPE_EXPLAIN'			=> 'Select the block type.',
-	'BLOCK_UPDATED'					=> 'The block was successfully updated.',
-
-	'MUST_SELECT_BLOCK'		=> 'You must select a block.',
-
-	'NO_BLOCK_HTML'		=> 'You have created block with template file but haven’t selected a template file for this block.',
-	'NO_BLOCK_TEXT'		=> 'You have created custom text block but haven’t entered the content for this block.',
-	'NO_BLOCK_TITLE'	=> 'You haven’t specified a title for the block.',
-
-	'SELECT_BLOCK_FILE'		=> 'Select a template file…',
-	'SELECT_BLOCK_ICON'		=> 'Select an icon…',
-	'SELECT_BLOCK_CLOCK'		=> 'Select an clock…',
-	'SELECT_BLOCK_POSITION'	=> 'Select a position…',
-	'SELECT_BLOCK_TYPE'		=> 'Select a type…',
-
-	// Manage links
-	'ACP_EXPRESS_LINKS_EXPLAIN'	=> 'Using this form you can add, edit, view and delete navigation links on the index page. You can also create special navigation links as categories which aren’t clickable links.',
-	'ADD_LINK'					=> 'Add new navigation link',
-	'ACP_PORTAL_MANAGE_LINKS'	=> 'Links verwalten',
-
-	'LINK_ADDED'		=> 'The navigation link was successfully added.',
-	'LINK_CAT'			=> 'Navigation link category',
-	'LINK_ICON'			=> 'Link icon',
-	'LINK_ICON_EXPLAIN'	=> 'Use this to define a small icon associated with the navigation link. The path is relative to the root phpBB directory.',
-	'LINK_IS_CAT'		=> 'Set as special link category',
-	'LINK_REMOVED'		=> 'The navigation link was successfully deleted.',
-	'LINK_TITLE'		=> 'Link title',
-	'LINK_UPDATED'		=> 'The navigation link was successfully updated.',
-	'LINK_URL'			=> 'Link URL',
-
-	'MUST_SELECT_LINK'	=> 'You must select a link.',
+	// Install
+	'PORTAL_BASIC_INSTALL'			=> 'Füge Basismodule hinzu',
+	'PORTAL_BASIC_UNINSTALL'		=> 'Entferne Module von Datenbank',
 	
-	'NO_LINK_TITLE'	=> 'You haven’t specified a title for the navigation link.',
-	'NO_LINK_URL'	=> 'You have created clickable navigation link but haven’t entered the URL for this navigation link.',
-
-	'SELECT_LINK_ICON'	=> 'Select an icon…',
-
-	// paypal
-	'PORTAL_PAY_ACC'							=> 'Paypal Account',
-	'PORTAL_PAY_ACC_EXPLAIN'					=> 'Gib deine e-mail-Adresse an, die du bei Paypal benutzt, z.B. xxx@xxx.com',
-));
-
-// BLOCK TITLES
-// Set additional block titles here...
-//
-// Example:
-// 'BLOCK_TOP_POSTERS'		=> 'Top posters',	/* Main block title */
-// 'BLOCK_TOP_POSTERS_SUB'	=> 'Posted',		/* Legend, block sub-title */
-//
-$lang = array_merge($lang, array(
-	'BLOCK_BIRTHDAY'				=> 'Geburtstage',	
-	'BLOCK_EXPRESS_LINKS'			=> 'Navigation',
-	'BLOCK_SEARCH'					=> 'Suche',
-	'BLOCK_CLOCK'					=> 'Uhr',
-	'BLOCK_USER_MENU'				=> 'Benutzer-Menü',
-	'BLOCK_MAIN_MENU'				=> '',
-	'BLOCK_CHANGE_STYLE'			=> 'Mein Board-Style',
-	'BLOCK_ONLINE'					=> 'Wer ist Online?',
-	'BLOCK_DONATION'				=> 'Paypal-Spenden',
-	'BLOCK_LINKS'					=> 'Links',
-	'BLOCK_LATEST_BOTS'				=> 'Bots',
-	'BLOCK_LATEST_MEMBERS'			=> 'Neueste Mitglieder',
-	'BLOCK_MINI_CALENDAR'			=> 'Kalender',
-	'BLOCK_ONLINE_FRIENDS'			=> 'Freunde',
-	'BLOCK_STATISTICS'				=> 'Statistik',
-	'BLOCK_TOP_POSTER'				=> 'Top Poster',
-	'BLOCK_CUSTOM'					=> 'Custom',
-	'BLOCK_BOTS'					=> 'Letzten Bots',
-));
-
-// CUSTOM PAGE TITLES
-// Set custom page titles here...
-//
-// Example:
-// 'PAGE_ABOUT'			=> 'About us',					/* Main page title */
-// 'PAGE_ABOUT_EXPLAIN'	=> 'Contact information here.',	/* Explanation, page sub-title */
-//
-$lang = array_merge($lang, array(
-));
-
-/**
-* A copy of Handyman` s MOD version check, to view it on the gallery overview
-*/
-$lang = array_merge($lang, array(
+	// Logs
+	'LOG_PORTAL_CONFIG'			=> '<strong>Portal-Einstellungen geändert</strong><br />&raquo; %s',
+	
+	/**
+	* A copy of Handyman` s MOD version check, to view it on the gallery overview
+	*/
 	'ANNOUNCEMENT_TOPIC'	=> 'Release Ankündigung',
 	'CURRENT_VERSION'		=> 'Derzeitige Version',
 	'DOWNLOAD_LATEST'		=> 'Neueste Version herunterladen',
@@ -160,6 +98,6 @@ $lang = array_merge($lang, array(
 	'RELEASE_ANNOUNCEMENT'	=> 'Ankündigungsthema',
 	'UP_TO_DATE'			=> '%s ist aktuell',
 	'VERSION_CHECK'			=> 'MOD Version Check',
+	
 ));
-
 ?>
