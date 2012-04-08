86eb3e75260d9f606e3c1d5112e92325b6c66e3c
Marc Alexander <admin@m-a-styles.de>
2012-04-08 17:03:44 +0200

    Make sure we can only move modules to allowed columns Moving a module to a column type (side & center columns) where this module already exists will cause massive issues

--- root/language/en/mods/info_acp_portal.php
+++ root/language/en/mods/info_acp_portal.php
@@ -52,6 +52,7 @@ $lang = array_merge($lang, array(
 	'MOVE_LEFT'						=> 'Move left',
 	'B3P_FILE_NOT_FOUND'			=> 'The requested file could not be found',
 	'UNABLE_TO_MOVE'				=> 'It is not possible to move the block to the selected column.',
+	'UNABLE_TO_MOVE_ROW'			=> 'It is not possible to move the block to the selected row.',
 	'DELETE_MODULE_CONFIRM'			=> 'Are you sure you wish to delete the module "%1$s"?',
 	'MODULE_RESET_SUCCESS'			=> 'Successfully reset the module settings.',
 	'MODULE_RESET_CONFIRM'			=> 'Are you sure you wish to reset the settings of the module "%1$s"?',
