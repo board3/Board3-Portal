c3066e6f015edd84f24b052574340a0a97133d9c
Marc Alexander <admin@m-a-styles.de>
2013-10-29 15:03:59 +0100

    [feature/tests] Add simple function test

---
+++ tests/functions/simple_test.php
@@ -0,0 +1,19 @@
+<?php
+/**
+*
+* @package testing
+* @copyright (c) Board3 Group ( www.board3.de )
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+require_once(dirname(__FILE__) . '/../../portal/portal/includes/functions.php');
+
+class phpbb_functions_simple_test extends PHPUnit_Framework_TestCase
+{
+	public function test_ap_validate()
+	{
+		$this->assertEquals('<br/>woot<br/><ul><li>test</li></ul>', ap_validate('<br />woot<br/><ul><li>test</li><br /></ul>'));
+	}
+
+}
