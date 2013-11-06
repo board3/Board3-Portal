9adf059eaa20d992093e76167d13af7b35d083bb
Marc Alexander <admin@m-a-styles.de>
2013-11-06 16:20:50 +0100

    [ticket/181] Correctly check if Board3 Portal extension is disabled

--- tests/functional/portal_redirect_test.php
+++ tests/functional/portal_redirect_test.php
@@ -23,17 +23,24 @@ class phpbb_functional_portal_redirect_test extends \board3\portal\tests\testfra
 
 	protected function enable_board3_portal_ext()
 	{
+		$enable_portal = false;
 		$crawler = self::request('GET', 'adm/index.php?i=acp_extensions&mode=main&sid=' . $this->sid);
-		$crawler->filter('tr.ext_disabled')->each(function ($node, $i)
+		$disabled_extensions = $crawler->filter('tr.ext_disabled')->extract(array('_text'));
+		foreach ($disabled_extensions as $extension)
 		{
-			if (strpos($node->text(), 'Board3 Portal') !== false)
+			if (strpos($extension, 'Board3 Portal') !== false)
 			{
-				$crawler = self::request('GET', 'adm/index.php?i=acp_extensions&mode=main&action=enable_pre&ext_name=board3%2fportal&sid=' . $this->sid);
-				$form = $crawler->selectButton('Enable')->form();
-				$crawler = self::submit($form);
-				$this->assertContains('The extension was enabled successfully', $crawler->text());
+				$enable_portal = true;
 			}
-		});
+		}
+
+		if ($enable_portal)
+		{
+			$crawler = self::request('GET', 'adm/index.php?i=acp_extensions&mode=main&action=enable_pre&ext_name=board3%2fportal&sid=' . $this->sid);
+			$form = $crawler->selectButton('Enable')->form();
+			$crawler = self::submit($form);
+			$this->assertContains('The extension was enabled successfully', $crawler->text());
+		}
 	}
 
 	public function test_redirect()
