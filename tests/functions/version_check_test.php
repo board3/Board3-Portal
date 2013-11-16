0c38ca292434ba1833f26e287636f1269a14c1f2
Marc Alexander <admin@m-a-styles.de>
2013-11-16 17:59:00 +0100

    [fix/version_check_test] Include utf_tools.php in version_check_test

--- tests/functions/version_check_test.php
+++ tests/functions/version_check_test.php
@@ -7,6 +7,8 @@
 *
 */
 
+require_once(dirname(__FILE__) . '/../../../../../includes/utf/utf_tools.php');
+
 class phpbb_functions_version_check_test extends \board3\portal\tests\testframework\test_case
 {
 	protected $version_check;
