73fa16f983cd9ddb570edc2dc63bfc4cf06fb067
Marc Alexander <admin@m-a-styles.de>
2013-11-17 00:38:36 +0100

    [feature/module_services] Move poll module to module services

--- migrations/v210_beta1.php
+++ migrations/v210_beta1.php
@@ -446,7 +446,7 @@ class v210_beta1 extends \phpbb\db\migration\migration
 				'module_status'		=> 1,
 			),
 			array(
-				'module_classname'		=> 'poll',
+				'module_classname'		=> '\board3\portal\modules\poll',
 				'module_column'		=> 2,
 				'module_order'		=> 5,
 				'module_name'		=> 'PORTAL_POLL',
