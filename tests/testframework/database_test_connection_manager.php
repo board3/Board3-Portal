4319d7d0417f13dcaa7fee5bba999397a29ee91c
Marc Alexander <admin@m-a-styles.de>
2014-04-05 14:49:59 +0200

    [ticket/224] Use new create_schema_files from phpBB and json schema
    B3P-224

--- tests/testframework/database_test_connection_manager.php
+++ tests/testframework/database_test_connection_manager.php
@@ -11,12 +11,12 @@ namespace board3\portal\tests\testframework;
 
 class database_test_connection_manager extends \phpbb_database_test_connection_manager
 {
-	public function load_schema()
+	public function load_schema($db)
 	{
 		$this->ensure_connected(__METHOD__);
 
 		$directory = dirname(__FILE__) . '/../schemas/';
-		$this->load_schema_from_file($directory);
+		$this->load_schema_from_file($directory, $db);
 
 	}
 }
