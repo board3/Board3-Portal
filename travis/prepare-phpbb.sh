fbf101d0ff0992b665b71083c2e2bb36292de8f4
Marc Alexander <admin@m-a-styles.de>
2014-06-11 16:30:57 +0200

    [ticket/261] Use correct branch for tests and use code and icc sniffer
    B3P-261

--- travis/prepare-phpbb.sh
+++ travis/prepare-phpbb.sh
@@ -21,4 +21,4 @@ cp -R . ../../tmp
 cd ../../
 
 # Clone phpBB
-git clone --depth=1 "git://github.com/nickvergessen/phpbb.git" "phpBB3" --branch=$BRANCH
+git clone --depth=1 "git://github.com/phpbb/phpbb.git" "phpBB3" --branch=$BRANCH
