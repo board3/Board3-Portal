ed33e19f4ba954606d4f8fe580a0114d4f39bb52
Marc Alexander <admin@m-a-styles.de>
2014-08-17 22:13:19 +0200

    [ticket/314] Check incorrect copyright lines

--- tests/unit/event/listener_test.php
+++ tests/unit/event/listener_test.php
@@ -2,7 +2,7 @@
 /**
 *
 * @package Quickedit
-* @copyright (c) 2014 Marc Alexander ( www.m-a-styles.de )
+* @copyright (c) 2014 Board3 Group ( www.board3.de )
 * @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
 *
 */
