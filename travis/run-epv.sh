dfc78cafd6bc1eae8a408680bd3f11a7ac7c8319
Marc Alexander <admin@m-a-styles.de>
2014-08-17 14:09:16 +0200

    [ticket/331] Add EPV check to travis builds
    B3P-331

---
+++ travis/run-epv.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+# This file is part of the Board3 Portal package.
+#
+# @copyright (c) Board3 Group <http://www.board3.de>
+# @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+#
+set -e
+set -x
+
+DB=$1
+TRAVIS_PHP_VERSION=$2
+EXTNAME=$3
+EPV=$4
+
+if [ "$TRAVIS_PHP_VERSION" == "5.5" -a "$DB" == "mysqli" -a "$EPV" == "1" ]
+then
+	phpBB/ext/$EXTNAME/vendor/bin/EPV.php run --dir='phpBB/ext/$EXTNAME/'
+fi
