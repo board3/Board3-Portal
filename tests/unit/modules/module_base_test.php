a1ec32579075d6e604b1093a075ce5f0399e4488
Marc Alexander <admin@m-a-styles.de>
2014-12-01 12:54:08 +0100

    [ticket/416] Add test for module base
    B3P-416

---
+++ tests/unit/modules/module_base_test.php
@@ -0,0 +1,34 @@
+<?php
+/**
+*
+* @package testing
+* @copyright (c) Board3 Group ( www.board3.de )
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+class phpbb_unit_modules_module_base_test extends \board3\portal\tests\testframework\test_case
+{
+	/** @var \board3\portal\modules\module_base */
+	protected $module_base;
+
+	public function setUp()
+	{
+		parent::setUp();
+
+		$this->module_base = new \board3\portal\modules\module_base();
+	}
+
+	public function test_get_templates()
+	{
+		$this->assertNull($this->module_base->get_template_side(5));
+		$this->assertNull($this->module_base->get_template_center(5));
+		$this->assertEquals(array(), $this->module_base->get_template_acp(5));
+	}
+
+	public function test_install()
+	{
+		$this->assertTrue($this->module_base->install(5));
+		$this->assertTrue($this->module_base->uninstall(5, null));
+	}
+}
