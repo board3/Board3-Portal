ef18373c9555ab5995ef22c30ade355bc900b702
Mauron <Mauron@users.noreply.github.com>
2015-01-17 05:24:27 +0100

    Update portal_whois_online_module.php

--- language/it/modules/portal_whois_online_module.php
+++ language/it/modules/portal_whois_online_module.php
@@ -32,5 +32,5 @@ if (empty($lang) || !is_array($lang))
 // equally where a string contains only two placeholders which are used to wrap text
 // in a url you again do not need to specify an order e.g., 'Click %sHERE%s' is fine
 $lang = array_merge($lang, array(
-	'PORTAL_WHOIS_ONLINE'			=> 'Chi c'è in linea',
+	'PORTAL_WHOIS_ONLINE'			=> 'Chi c\'è in linea',
 ));
