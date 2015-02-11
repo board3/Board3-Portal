9bf8fd049d54ccf15362ded31d3739ac9e538fd7
Marc Alexander <admin@m-a-styles.de>
2015-02-11 11:53:19 +0100

    [ticket/465] Use path instead of pattern in routing.yml
    B3P-465

--- tests/functional/portal_no_error_test.php
+++ tests/functional/portal_no_error_test.php
@@ -28,7 +28,18 @@ class phpbb_functional_portal_no_error_test extends \board3\portal\tests\testfra
 	public function test_enable_subsilver()
 	{
 		$crawler = self::request('GET', 'adm/index.php?i=acp_styles&mode=install&sid=' . $this->sid);
-		$link = $crawler->selectLink('Install style')->link()->getUri();
+		$links = $crawler->filter('.actions a');
+		$link = $links->selectLink('Install style')->link()->getUri();
+		for ($i = 0; $i < sizeof($links); $i++)
+		{
+			$link = $links->eq($i)->selectLink('Install style')->link()->getUri();
+
+			if (strpos($link, 'subsilver2') !== false)
+			{
+				break;
+			}
+		}
+
 		$crawler = self::request('GET', substr($link, strpos($link, '/adm') + 1));
 		$this->assertContains('Style "subsilver2" has been installed', $crawler->text());
 	}
